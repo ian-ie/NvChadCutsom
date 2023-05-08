8c277fccc078da14b00d3a3e5fa7fca366f0b02c
ianie <2817181586@qq.com>
2023-05-08 21:40:18 +0800

    header font

--- init.lua
+++ init.lua
@@ -12,7 +12,7 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 
 if vim.fn.exists "g:neovide" then
-    vim.opt.guifont = { "JetBrains Mono", ":h12" }
+    vim.opt.guifont = { "JetBrains Mono, 等距更纱黑体 SC:h12" }
     vim.g.neovide_transparency = 0.95
-    vim.g.neovide_cursor_vfx_mode = "ripple"
+    vim.g.neovide_cursor_vfx_mode = "railgun"
 end
