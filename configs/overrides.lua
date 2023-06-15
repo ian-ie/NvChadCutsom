69e208c76b074d4888207903831cce8f69e8adaa
ianie <2817181586@qq.com>
2023-06-15 23:57:38 +0800

    rainbow color

--- configs/overrides.lua
+++ configs/overrides.lua
@@ -55,6 +55,15 @@ M.treesitter = {
         -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
         extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
         max_file_lines = nil, -- Do not enable for files with more than n lines, int
+        colors = {
+            "#E16C75",
+            "#E5C07B",
+            "#98C379",
+            "#56B6C2",
+            "#61AFEF",
+            "#C678DD",
+            "#E16C75",
+        },
         -- colors = {}, -- table of hex strings
         -- termcolors = {} -- table of colour name strings
     },
